639311554f048ac98e497f9fde18c33deae27d8d
Danel Tabada <daneltabada@gmail.com>
2019-02-17 12:02:04 +0800

    set-up google api

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,6 +4,8 @@ const GoogleStrategy = require('passport-google-oauth20');
 passport.use(
     new GoogleStrategy({
         //options for the google strat
+        clientID:'640011586518-6hs4igerst4g8630fm3dkh1du78rfvp0.apps.googleusercontent.com',
+        clientSecret:'jrvG5ZCcS46UsN6pqpeZ3LkF'
     }), () => {
         //passport callback function
 });
