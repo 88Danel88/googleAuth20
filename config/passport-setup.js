7f51e2271f059a2eb147ebecc94853727062f54f
Danel Tabada <daneltabada@gmail.com>
2019-02-17 14:05:56 +0800

    set-up db connection and models

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,11 +1,27 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./keys');
+const User = require('../models/user-models');
 
 passport.use(
     new GoogleStrategy({
         //options for the google strat
-        clientID:'640011586518-6hs4igerst4g8630fm3dkh1du78rfvp0.apps.googleusercontent.com',
-        clientSecret:'jrvG5ZCcS46UsN6pqpeZ3LkF'
-    }), () => {
-        //passport callback function
-});
+        callbackURL:'/auth/google/redirect',
+        clientID:keys.google.clientID,
+        clientSecret:keys.google.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
+        //check if user already exist in our db
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser){
+                console.log('user has visited our site');
+            }else{
+                new User({
+                    username: profile.displayName,
+                    googleId: profile.id
+                }).save().then((newUser) => {
+                    console.log('new user created:' + newUser);
+                });
+            }
+        })
+    })
+);
