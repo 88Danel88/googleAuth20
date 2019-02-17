443880a6a5548d0cb16f353bd2b09a49db84a52b
Danel Tabada <daneltabada@gmail.com>
2019-02-17 11:04:52 +0800

    added routes

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const authRoutes = require('./routes/auth-routes');
+
 const app = express();
 
+//set-up view engine
 app.set('view engine','ejs');
 
+//set-up route
+app.use('/auth',authRoutes)
+
+//index route
 app.get('/',(req, res)=>{
     console.log('HEY!!!');
     res.render('index');
 });
 
+//listen to a port
 app.listen(8000,()=>{
     console.log('listening at port 8000')
 });
